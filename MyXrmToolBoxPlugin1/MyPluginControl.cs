7f7b5eadbc6ea529178ed75d15111f045971cef4
alazydeveloperhub <56925584+alazydeveloperhub@users.noreply.github.com>
2019-10-23 10:10:16 -0400

    Add project files.

---
+++ MyXrmToolBoxPlugin1/MyPluginControl.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Windows.Forms;
+using XrmToolBox.Extensibility;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk;
+using McTools.Xrm.Connection;
+using System.Web.UI.WebControls;
+using Microsoft.Crm.Sdk.Messages;
+using System.Collections.Generic;
+
+namespace SecurityRolesSync
+{
+    public partial class MyPluginControl : PluginControlBase
+    {
+        private Settings mySettings;
+
+        public MyPluginControl()
+        {
+            InitializeComponent();
+        }
+
+        private void MyPluginControl_Load(object sender, EventArgs e)
+        {
+            ShowInfoNotification("This is a notification that can lead to XrmToolBox repository", new Uri("https://github.com/MscrmTools/XrmToolBox"));
+
+            // Loads or creates the settings for the plugin
+            if (!SettingsManager.Instance.TryLoad(GetType(), out mySettings))
+            {
+                mySettings = new Settings();
+
+                LogWarning("Settings not found => a new settings file has been created!");
+            }
+            else
+            {
+                LogInfo("Settings found and loaded");
+            }
+            ExecuteMethod(GetUsers);
+        }
+
+        private void tsbClose_Click(object sender, EventArgs e)
+        {
+            CloseTool();
+        }
+
+
+        private void GetUsers()
+        {
+            WorkAsync(new WorkAsyncInfo
+            {
+                // Message = "Getting accounts",
+                Work = (worker, args) =>
+                {
+                    ConditionExpression condition1 = new ConditionExpression();
+                    condition1.AttributeName = "isdisabled";
+                    condition1.Operator = ConditionOperator.Equal;
+                    condition1.Values.Add("false");
+
+                    FilterExpression filter1 = new FilterExpression();
+                    filter1.Conditions.Add(condition1);
+
+                    QueryExpression query = new QueryExpression("systemuser");
+                    query.ColumnSet.AddColumns("systemuserid", "fullname");
+                    query.Criteria.AddFilter(filter1);
+
+                    args.Result = Service.RetrieveMultiple(query);
+
+                },
+                PostWorkCallBack = (args) =>
+                {
+                    if (args.Error != null)
+                    {
+                        MessageBox.Show(args.Error.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    var result = args.Result as EntityCollection;
+                    if (result != null)
+                    {
+                        var dropDownListCollection = new ListItemCollection();
+                        var dropDownListCollection1 = new ListItemCollection();
+
+                        for (int i = 0; i < result.Entities.Count; i++)
+                        {
+                            dropDownListCollection.Add(new ListItem(result.Entities[i]["fullname"].ToString(), result.Entities[i]["systemuserid"].ToString()));
+                            dropDownListCollection1.Add(new ListItem(result.Entities[i]["fullname"].ToString(), result.Entities[i]["systemuserid"].ToString()));
+                        }
+                        Source.DataSource = dropDownListCollection;
+                        Source.DisplayMember = "Text";
+                        Source.ValueMember = "Value";
+
+                        comboBox1.DataSource = dropDownListCollection1;
+                        comboBox1.DisplayMember = "Text";
+                        comboBox1.ValueMember = "Value";
+
+                    }
+                }
+            });
+        }
+
+        /// <summary>
+        /// This event occurs when the plugin is closed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MyPluginControl_OnCloseTool(object sender, EventArgs e)
+        {
+            // Before leaving, save the settings
+            SettingsManager.Instance.Save(GetType(), mySettings);
+        }
+
+        /// <summary>
+        /// This event occurs when the connection has been updated in XrmToolBox
+        /// </summary>
+        public override void UpdateConnection(IOrganizationService newService, ConnectionDetail detail, string actionName, object parameter)
+        {
+            base.UpdateConnection(newService, detail, actionName, parameter);
+
+            if (mySettings != null && detail != null)
+            {
+                mySettings.LastUsedOrganizationWebappUrl = detail.WebApplicationUrl;
+                LogInfo("Connection has changed to: {0}", detail.WebApplicationUrl);
+            }
+        }
+
+        private void syncRoles_Click(object sender, EventArgs e)
+        {
+            Guid id = new Guid(Source.SelectedValue.ToString());
+            Guid TargerUserId =new Guid(comboBox1.SelectedValue.ToString());
+
+            WorkAsync(new WorkAsyncInfo
+            {
+                Message = "Syncing Security Roles from Source to Target...",
+                Work = (w, ar) =>
+                {
+                    // This code is executed in another thread
+                   
+                    
+                    List<Guid> sourceRoleIds;
+                    Guid SourceBuId;
+                    GetUserRoles(id, out sourceRoleIds, out SourceBuId);
+
+                    List<Guid> targetRoleIds;
+                    Guid targetBuId;
+                    GetUserRoles(TargerUserId, out targetRoleIds, out targetBuId);
+
+                    if (SourceBuId == targetBuId)
+                    {
+                        EntityReferenceCollection targetRoleCollection = new EntityReferenceCollection();
+                        foreach (var tr in targetRoleIds)
+                        {
+                            targetRoleCollection.Add(new EntityReference("role", tr));
+                        }
+                        //clear all the sec roles of target user
+                        Service.Disassociate(
+                                            "systemuser",
+                                            TargerUserId,
+                                            new Relationship("systemuserroles_association"),
+                                            targetRoleCollection);
+                    }
+                    else
+                    {
+
+                        SetBusinessSystemUserRequest request = new SetBusinessSystemUserRequest();
+                        request.BusinessId = SourceBuId;
+                        request.UserId = TargerUserId;
+                        request.ReassignPrincipal = new EntityReference("systemuser", TargerUserId);
+                        SetBusinessSystemUserResponse response =
+                        (SetBusinessSystemUserResponse)Service.Execute(request);
+                    }
+
+                    //Adding Bu and Roles to the target user
+
+                    EntityReferenceCollection roleCollection = new EntityReferenceCollection();
+                    foreach (var r in sourceRoleIds)
+                    {
+                        roleCollection.Add(new EntityReference("role", r));
+                    }
+
+                    Service.Associate("systemuser", TargerUserId,
+                                       new Relationship("systemuserroles_association"),
+                                        roleCollection);
+                },
+           
+                PostWorkCallBack = ar =>
+                {
+                    // This code is executed in the main thread
+                    MessageBox.Show($"Security Roles synced.");
+                },
+                AsyncArgument = null,
+                // Progress information panel size
+                MessageWidth = 340,
+                MessageHeight = 150
+            });
+            
+        }
+
+        private void GetUserRoles(Guid userid, out List<Guid> roleIds, out Guid buId)
+        {
+            QueryExpression qe = new QueryExpression("systemuserroles");
+            qe.ColumnSet.AddColumns("systemuserid");
+            qe.Criteria.AddCondition("systemuserid", ConditionOperator.Equal, userid);
+
+            LinkEntity link1 = qe.AddLink("systemuser", "systemuserid", "systemuserid", JoinOperator.Inner);
+            link1.Columns.AddColumns("fullname", "internalemailaddress", "businessunitid");
+            LinkEntity link = qe.AddLink("role", "roleid", "roleid", JoinOperator.Inner);
+            link.Columns.AddColumns("roleid", "name");
+            EntityCollection results = Service.RetrieveMultiple(qe);
+
+            roleIds = new List<Guid>();
+            buId = new Guid();
+            foreach (var c in results.Entities)
+            {
+                EntityReference businessunit = (EntityReference)(((AliasedValue)c["systemuser1.businessunitid"]).Value);
+                buId = businessunit.Id;
+                roleIds.Add(new Guid(((AliasedValue)c["role2.roleid"]).Value.ToString()));
+
+            }
+        }
+
+       
+    }
+}
---
+++ MyXrmToolBoxPlugin1/MyPluginControl.designer.cs
@@ -0,0 +1,143 @@
+﻿namespace SecurityRolesSync
+{
+    partial class MyPluginControl
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStripMenu = new System.Windows.Forms.ToolStrip();
+            this.tsbClose = new System.Windows.Forms.ToolStripButton();
+            this.tssSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.syncRoles = new System.Windows.Forms.Button();
+            this.Source = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toolStripMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripMenu
+            // 
+            this.toolStripMenu.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStripMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbClose,
+            this.tssSeparator1});
+            this.toolStripMenu.Location = new System.Drawing.Point(0, 0);
+            this.toolStripMenu.Name = "toolStripMenu";
+            this.toolStripMenu.Padding = new System.Windows.Forms.Padding(0, 0, 2, 0);
+            this.toolStripMenu.Size = new System.Drawing.Size(902, 32);
+            this.toolStripMenu.TabIndex = 4;
+            this.toolStripMenu.Text = "toolStrip1";
+            // 
+            // tsbClose
+            // 
+            this.tsbClose.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsbClose.Name = "tsbClose";
+            this.tsbClose.Size = new System.Drawing.Size(129, 29);
+            this.tsbClose.Text = "Close this tool";
+            this.tsbClose.Click += new System.EventHandler(this.tsbClose_Click);
+            // 
+            // tssSeparator1
+            // 
+            this.tssSeparator1.Name = "tssSeparator1";
+            this.tssSeparator1.Size = new System.Drawing.Size(6, 32);
+            // 
+            // syncRoles
+            // 
+            this.syncRoles.Location = new System.Drawing.Point(361, 168);
+            this.syncRoles.Name = "syncRoles";
+            this.syncRoles.Size = new System.Drawing.Size(144, 72);
+            this.syncRoles.TabIndex = 8;
+            this.syncRoles.Text = "Sync Security Roles";
+            this.syncRoles.UseVisualStyleBackColor = true;
+            this.syncRoles.Click += new System.EventHandler(this.syncRoles_Click);
+            // 
+            // Source
+            // 
+            this.Source.FormattingEnabled = true;
+            this.Source.Location = new System.Drawing.Point(32, 104);
+            this.Source.Name = "Source";
+            this.Source.Size = new System.Drawing.Size(278, 28);
+            this.Source.TabIndex = 9;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(558, 104);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(289, 28);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Source Sec Roles";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(554, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Target User";
+            // 
+            // MyPluginControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Source);
+            this.Controls.Add(this.syncRoles);
+            this.Controls.Add(this.toolStripMenu);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MyPluginControl";
+            this.Size = new System.Drawing.Size(902, 528);
+            this.Load += new System.EventHandler(this.MyPluginControl_Load);
+            this.toolStripMenu.ResumeLayout(false);
+            this.toolStripMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip toolStripMenu;
+        private System.Windows.Forms.ToolStripButton tsbClose;
+        private System.Windows.Forms.ToolStripSeparator tssSeparator1;
+        private System.Windows.Forms.Button syncRoles;
+        private System.Windows.Forms.ComboBox Source;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
